a4cac927b9cc3030b6b8d46adbfe724122c2c7d7
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-05-21 10:27:29 -0500

    updated portfolio with one project, and added webpage and github links

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,6 +15,9 @@ class Portfolio extends Component {
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
             <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <div style={{ textAlign: "center" }}>{projects.category}</div>
+            <a style={{ textAlign: "center" }} href={projects.deployed}>Deployed Webpage</a>
+            <a style={{ textAlign: "center" }} href={projects.project_github}>Project GitHub</a>
           </div>
         </div>
       );
